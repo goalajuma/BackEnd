0ff785f39b3c7927062cefa3214329738ac0adb7
jinwooseok <jinus7949@naver.com>
2024-01-03 22:43:42 +0900

    feat:좋아요 엔티티 추가

---
+++ src/main/java/com/kakao/golajuma/comment/persistence/entity/LikeyEntity.java
@@ -0,0 +1,35 @@
+package com.kakao.golajuma.comment.persistence.entity;
+
+import com.kakao.golajuma.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+import static com.kakao.golajuma.comment.persistence.entity.CommentEntity.ENTITY_PREFIX;
+
+@Getter
+@SuperBuilder(toBuilder = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
+@Table(name = ENTITY_PREFIX)
+public class LikeyEntity extends BaseEntity {
+
+    public static final String ENTITY_PREFIX = "likey";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ENTITY_PREFIX + "_id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name = ENTITY_PREFIX + "_user_id")
+    private Long userId;
+
+    @Column(name = ENTITY_PREFIX + "_comment_id")
+    private Long commentId;
+
+}
