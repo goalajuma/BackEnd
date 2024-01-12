ea871d45b572a32dc98fc90eebea6167f1c0daae
PHS00 <hyunsoo159487@naver.com>
2024-01-12 15:17:47 +0900

    feat : (#143) 진행중인 투표 예외 추가

---
+++ src/main/java/com/kakao/golajuma/vote/domain/exception/vote/OnException.java
@@ -0,0 +1,12 @@
+package com.kakao.golajuma.vote.domain.exception.vote;
+
+import com.kakao.golajuma.common.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class OnException extends BusinessException {
+    private static final String MESSAGE = "아직 진행중인 투표입니다.";
+
+    public OnException() {
+        super(MESSAGE, HttpStatus.BAD_REQUEST);
+    }
+}
