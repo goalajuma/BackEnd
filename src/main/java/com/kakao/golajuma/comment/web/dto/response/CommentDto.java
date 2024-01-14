2fc3bf1b540b2c4be3ea736c08aabdb0b7180179
jinwooseok <jinus7949@naver.com>
2024-01-14 13:44:22 +0900

    refactor : commentDto likey추가

--- src/main/java/com/kakao/golajuma/comment/web/dto/response/CommentDto.java
+++ src/main/java/com/kakao/golajuma/comment/web/dto/response/CommentDto.java
@@ -4,24 +4,24 @@ import com.kakao.golajuma.comment.persistence.entity.CommentEntity;
 import com.kakao.golajuma.common.marker.AbstractResponseDto;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 public class CommentDto implements AbstractResponseDto {
-	private final Long id;
-	private final Boolean isOwner;
-	private final String username;
-	private final String content;
-	private final LocalDateTime createTime;
-	/**
-	 * 해당 클래스의 객체 생성자
-	 *
-	 * @implSpec 이 클래스의 하위 클래스들은 각각의 요소를 이 클래스와 같은 요소들로 생성한다.
-	 */
-	public CommentDto(CommentEntity entity, Boolean isOwner, String username) {
+	private Long id;
+	private Boolean isOwner;
+	private String username;
+	private String content;
+	private LocalDateTime createTime;
+	private int likey;
+
+	public CommentDto(CommentEntity entity, Boolean isOwner, String username, int likey) {
 		this.id = entity.getId();
 		this.isOwner = isOwner;
 		this.username = username;
 		this.content = entity.getContent();
 		this.createTime = entity.getUpdatedDate();
+		this.likey = likey;
 	}
 }
