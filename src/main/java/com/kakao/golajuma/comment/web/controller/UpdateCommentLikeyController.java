d441eb8244dadf95780775e0924b4b8c82f07cf2
jinwooseok <jinus7949@naver.com>
2024-01-14 13:48:34 +0900

    refactor (#140) : 대댓글 컨트롤러, 공감변경 컨트롤러 생성

--- src/main/java/com/kakao/golajuma/comment/web/controller/UpdateCommentLikeyController.java
+++ src/main/java/com/kakao/golajuma/comment/web/controller/UpdateCommentLikeyController.java
@@ -1,20 +1,29 @@
 package com.kakao.golajuma.comment.web.controller;
 
+import com.kakao.golajuma.auth.web.support.Login;
+import com.kakao.golajuma.comment.domain.service.UpdateCommentLikeyService;
+import com.kakao.golajuma.comment.web.dto.request.CreateCommentReplyRequest;
+import com.kakao.golajuma.comment.web.dto.response.CreateCommentReplyResponse;
+import com.kakao.golajuma.comment.web.dto.response.UpdateCommentResponse;
 import com.kakao.golajuma.common.support.respnose.ApiResponse;
+import com.kakao.golajuma.common.support.respnose.ApiResponseBody;
 import com.kakao.golajuma.common.support.respnose.ApiResponseGenerator;
 import com.kakao.golajuma.common.support.respnose.MessageCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/comments/{commentId}/likey")
 public class UpdateCommentLikeyController {
+    
+    private final UpdateCommentLikeyService updateCommentLikeyService;
     @PostMapping
-    public ApiResponse<?> updateCommentLikey(){
+    public ApiResponse<ApiResponseBody.SuccessBody<Void>> updateCommentLikey(@PathVariable Long commentId, @Login Long userId) {
+        updateCommentLikeyService.execute(commentId, userId);
         return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.UPDATE);
     }
 }
