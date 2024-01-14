cf0c8a4c377456400a4e433c6bb527b1216ec5e3
jinwooseok <jinus7949@naver.com>
2024-01-14 13:45:35 +0900

    refactor : 기존response likey 추가

--- src/main/java/com/kakao/golajuma/comment/web/dto/response/CreateCommentResponse.java
+++ src/main/java/com/kakao/golajuma/comment/web/dto/response/CreateCommentResponse.java
@@ -3,7 +3,7 @@ package com.kakao.golajuma.comment.web.dto.response;
 import com.kakao.golajuma.comment.persistence.entity.CommentEntity;
 
 public class CreateCommentResponse extends CommentDto {
-	public CreateCommentResponse(CommentEntity commentEntity, Boolean isOwner, String username) {
-		super(commentEntity, isOwner, username);
-	}
+    public CreateCommentResponse(CommentEntity commentEntity, Boolean isOwner, String username, int likey) {
+        super(commentEntity, isOwner, username, likey);
+    }
 }
