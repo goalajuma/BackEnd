e788c50d270bec86a39fc33595a14b6c9e8d459d
jinwooseok <jinus7949@naver.com>
2024-01-14 13:46:54 +0900

    refactor : 대댓글 request response추가

---
+++ src/main/java/com/kakao/golajuma/comment/web/dto/response/CreateCommentReplyResponse.java
@@ -0,0 +1,9 @@
+package com.kakao.golajuma.comment.web.dto.response;
+
+import com.kakao.golajuma.comment.persistence.entity.CommentEntity;
+
+public class CreateCommentReplyResponse extends CommentDto {
+    public CreateCommentReplyResponse(CommentEntity commentEntity, Boolean isOwner, String username, int likey) {
+        super(commentEntity, isOwner, username, likey);
+    }
+}
