e3a0e9acba199db502ae4b2870e63c5d04bc5d1a
PHS00 <hyunsoo159487@naver.com>
2024-01-18 23:12:06 +0900

    feat : (#152) 검색 기록 응답 dto 추가

---
+++ src/main/java/com/kakao/golajuma/vote/web/dto/response/HistoryResponse.java
@@ -0,0 +1,43 @@
+package com.kakao.golajuma.vote.web.dto.response;
+
+import com.kakao.golajuma.common.marker.AbstractResponseDto;
+import com.kakao.golajuma.vote.persistence.entity.HistoryEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class HistoryResponse implements AbstractResponseDto {
+    List<Keyword> keywords;
+
+    public HistoryResponse(List<Keyword> keywords){
+        this.keywords = keywords;
+    }
+
+    @Getter
+    @Builder
+    public static class Keyword{
+        private Long id;
+        private String keyword;
+
+        public static Keyword convert(Long id, String keyword){
+            return Keyword.builder()
+                    .id(id)
+                    .keyword(keyword)
+                    .build();
+        }
+    }
+
+    public static HistoryResponse convert(List<HistoryEntity> histories){
+        List<Keyword> keywords = new ArrayList<>();
+        for(HistoryEntity history : histories){
+            Keyword keyword = Keyword.convert(history.getId(), history.getKeyword());
+            keywords.add(keyword);
+        }
+        return new HistoryResponse(keywords);
+    }
+}
