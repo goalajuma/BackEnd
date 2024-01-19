2ff3b8b70bd2fd1c1dace9a7ec5167e874d6e684
PHS00 <hyunsoo159487@naver.com>
2024-01-19 23:24:22 +0900

    refactor : (#152) 투표 검색 기록 조회 오류 수정

--- src/main/java/com/kakao/golajuma/vote/domain/service/GetHistoryService.java
+++ src/main/java/com/kakao/golajuma/vote/domain/service/GetHistoryService.java
@@ -4,6 +4,8 @@ import com.kakao.golajuma.vote.persistence.entity.HistoryEntity;
 import com.kakao.golajuma.vote.persistence.repository.HistoryRepository;
 import com.kakao.golajuma.vote.web.dto.response.HistoryResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +18,8 @@ public class GetHistoryService {
     private final HistoryRepository historyRepository;
 
     public HistoryResponse execute(Long userId){
-        List<HistoryEntity> history = historyRepository.findAllByUserId(userId);
+        Pageable pageable = PageRequest.of(0, 5);
+        List<HistoryEntity> history = historyRepository.findAllByUserId(userId, pageable);
 
         return HistoryResponse.convert(history);
     }
