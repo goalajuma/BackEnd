0bf6221f7eb47e638b9c24f2a03e0416077e35ff
PHS00 <hyunsoo159487@naver.com>
2024-01-20 22:43:14 +0900

    feat : (#154) 투표 옵션 추가 서비스 추가

---
+++ src/main/java/com/kakao/golajuma/vote/domain/service/AddOptionService.java
@@ -0,0 +1,38 @@
+package com.kakao.golajuma.vote.domain.service;
+
+import com.kakao.golajuma.vote.domain.exception.vote.NotFoundVoteException;
+import com.kakao.golajuma.vote.domain.exception.vote.NotWriterException;
+import com.kakao.golajuma.vote.persistence.entity.OptionEntity;
+import com.kakao.golajuma.vote.persistence.entity.VoteEntity;
+import com.kakao.golajuma.vote.persistence.repository.OptionRepository;
+import com.kakao.golajuma.vote.persistence.repository.VoteRepository;
+import com.kakao.golajuma.vote.util.ImageUploader;
+import com.kakao.golajuma.vote.web.dto.request.AddOptionRequest;
+import com.kakao.golajuma.vote.web.dto.request.RequestOptionDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class AddOptionService {
+
+    private final VoteRepository voteRepository;
+    private final OptionRepository optionRepository;
+    private final ImageUploader imageUploader;
+
+    public void execute(AddOptionRequest request, Long voteId, Long userId){
+        VoteEntity voteEntity = voteRepository.findById(voteId).orElseThrow(NotFoundVoteException::new);
+
+        if(!voteEntity.isOwner(userId)){
+            throw new NotWriterException();
+        }
+
+        for (RequestOptionDto optionDto : request.getOptions()) {
+            OptionEntity optionEntity = OptionEntity.create(imageUploader, optionDto, voteId);
+            optionRepository.save(optionEntity);
+        }
+    }
+
+}
