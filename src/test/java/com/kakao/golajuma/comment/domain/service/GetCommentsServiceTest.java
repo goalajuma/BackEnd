1be74501d2ac6a8244f11b78ca602bcea83a17e0
jinwooseok <jinus7949@naver.com>
2024-01-28 11:15:28 +0900

    refactor (#163) : 댓글조회 오류 수정

--- src/test/java/com/kakao/golajuma/comment/domain/service/GetCommentsServiceTest.java
+++ src/test/java/com/kakao/golajuma/comment/domain/service/GetCommentsServiceTest.java
@@ -1,13 +1,17 @@
 package com.kakao.golajuma.comment.domain.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import com.kakao.golajuma.comment.persistence.entity.CommentEntity;
 import com.kakao.golajuma.comment.persistence.repository.CommentRepository;
-import com.kakao.golajuma.comment.web.dto.response.CommentDto;
+import com.kakao.golajuma.comment.persistence.repository.LikeyRepository;
 import com.kakao.golajuma.comment.web.dto.response.GetCommentsResponse;
+import com.kakao.golajuma.comment.web.dto.response.ParentCommentDto;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
@@ -25,6 +29,8 @@ class GetCommentsServiceTest {
 
 	@Mock private GetUserNameService getUserNameService;
 
+	@Mock private LikeyRepository likeyRepository;
+
 	@Test
 	@DisplayName("유저가 댓글 리스트를 호출하는데 성공한다.")
 	void get_comment_list_success_test() {
@@ -33,19 +39,31 @@ class GetCommentsServiceTest {
 		Long voteId = 1L;
 
 		CommentEntity commentEntity1 =
-				CommentEntity.builder().voteId(voteId).userId(userId).content("content1").build();
+				CommentEntity.builder()
+						.voteId(voteId)
+						.userId(userId)
+						.content("content1")
+						.anonymous(true)
+						.build();
 		CommentEntity commentEntity2 =
-				CommentEntity.builder().voteId(voteId).userId(userId).content("content2").build();
-
+				CommentEntity.builder()
+						.voteId(voteId)
+						.userId(userId)
+						.content("content2")
+						.anonymous(true)
+						.build();
+		Map<Long, String[]> usernameMap = new HashMap<>();
+		usernameMap.put(userId, new String[] {"tester", "익명1"});
 		List<CommentEntity> commentEntities =
 				Stream.of(commentEntity1, commentEntity2).collect(Collectors.toList());
 
 		when(commentRepository.findByVoteId(voteId)).thenReturn(commentEntities);
-		when(getUserNameService.execute(userId)).thenReturn("tester");
+		when(getUserNameService.execute(commentEntities)).thenReturn(usernameMap);
+		when(likeyRepository.countByCommentId(any())).thenReturn(2);
 		// when
 		GetCommentsResponse response = getCommentsService.execute(voteId, userId);
 		// then
-		assertThat(response.getComments().get(0).getClass()).isEqualTo(CommentDto.class);
+		assertThat(response.getComments().get(0).getClass()).isEqualTo(ParentCommentDto.class);
 
 		assertThat(response.getComments().get(0).getContent()).isEqualTo("content1");
 		assertThat(response.getComments().get(1).getContent()).isEqualTo("content2");
